aa6136ff24b644e1ebe190e6f0d803bf556efd86
Arav Pandey <aravpandey3010@gmail.com>
2025-11-22 19:44:49 -0500

    Updated Main File

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,12 @@
+if (!crypto.randomUUID) {
+  (crypto as any).randomUUID = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  };
+}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -22,3 +31,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </React.StrictMode>,
 )
+
